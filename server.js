2d00e4de56e3e8959be4ae95538bce160804eab1
Matt Desmet <mattd.email@gmail.com>
2017-03-16 05:32:47 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+var path = require('path');
+var bodyParser = require('body-parser');
+
+var PORT = 8000
+
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, './client'))); //MUST
+
+app.listen(PORT, function(){
+	console.log(`Listening on port ${PORT}`)
+})
