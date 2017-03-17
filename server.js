8af0d5727ef326cc1b8c2e1051d4dfc4b18776ba
Matt Desmet <mattd.email@gmail.com>
2017-03-17 07:46:11 -0700

    updated server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 var PORT = 8000
 
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, './client'))); //MUST
+app.use(express.static(path.join(__dirname, './client')));
 
 app.listen(PORT, function(){
 	console.log(`Listening on port ${PORT}`)
